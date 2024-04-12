ff0a0b34274448725f872debb051e0d984d0c6ef
Pratik Pawar <pawarpratikmm25@gmail.com>
2024-04-12 21:03:54 +0530

    Updated Files

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # used to install local package in virtual environment
+from setuptools import find_packages,setup
+
+setup(
+    name = 'MCQ_Generator', #name of package
+    version = '0.0.1',
+    author = 'pratik pawar',
+    author_email = 'pawarpratikmm25@gmail.com',
+    install_requirements = ['google-generativeai','langchain_google_genai','langchain','streamlit','python-dotenv','PyPDF2'],
+    packages = find_packages()
+)
